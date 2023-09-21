eaff8d007d701bca9e7ac8272c08f72bacf8cbee
nandlalsakle <143082186+nandlalsakle@users.noreply.github.com>
2023-09-21 09:44:12 +0000

    change in location of file

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,48 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue =100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log(id===anotherId);
+
+const bigNumber = 342443473686362n;
+
+
+
+//Reference (Non primitive)
+
+//Array, Objects, Funtions
+
+const heros = ["shaktiman", "naagraj", "doga"]; //Arrays
+// console.log(typeof heros);
+
+
+
+let myObj = {           //objects
+    name : "Nandlal",
+    age : 22,
+}
+
+
+const myFunction = function () {
+    console.log("Hello world");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+console.log(typeof myObj);
+console.log(typeof heros);
+console.log(typeof scoreValue);
+console.log(typeof outsideTemp);
+console.log(typeof anotherId);
+console.log(typeof isLoggedIn);
+console.log(typeof userEmail);
