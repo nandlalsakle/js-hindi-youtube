72427671f813ac9bb934ca3ba4a28a9ea775863a
nandlalsakle <143082186+nandlalsakle@users.noreply.github.com>
2023-09-21 08:00:21 +0000

    conversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -23,5 +23,35 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 
 let someNumber = 34;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//**************** Operations **********************/
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue); //-3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " Nandlal";
+let str3 = str1+str2;
+//console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(3+4*5%3);
+
+// console.log(true); // true
+// console.log(+true); // 1
+//console.log(true+); //error
+console.log(+""); // 0
